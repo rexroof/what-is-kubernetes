1aa9d17951da16bbb9cca30de5c962f2b6976081
Rex Roof <rexroof@gmail.com>
2021-02-10 08:32:37 -0500

    kubens/kubectx aliases not available

--- intro/demo.sh
+++ intro/demo.sh
@@ -3,17 +3,24 @@ set -e
 
 minikube start --driver=docker
 sleep 10
-kubectx minikube
-kubectl --context=minikube create namespace demo
-kubens demo
+kubectl ctx minikube
+kubectl --context=minikube create namespace demo || true
+kubectl ns demo
 kubectl --context=minikube apply -n demo -f demos/
 
+
+# start watch get pods in main terminal?
+
+# minikube kind
+# yaml
 # deployment 
-# nodes
 # kubectl get / describe
+# nodes
 # pods
 # services
 
 
+cd demos
+
 # always do this:
-exec bash
+exec zsh
